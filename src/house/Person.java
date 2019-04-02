6d5314a3a2fc83b732e3ac2da1d7ccddb92d0673
Gabriel Marchiz <mihaigabrielmarchizrgmail.com>
2019-04-02 20:02:58 +0200

    antesconflicto

--- src/house/Person.java
+++ src/house/Person.java
@@ -1,11 +1,11 @@
 package house;
-
+///////////////ahshdqwfsnweoif
 import java.awt.*;
 
 /**
  * A person that can be manipulated and that draws itself on a canvas.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael Kï¿½lling and David J. Barnes
  * @version 2016.02.29
  */
 
