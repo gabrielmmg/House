9f29bfa5b7a224c58bb165d047dbce7fbc7f06b3
Gabriel Marchiz <mihaigabrielmarchizrgmail.com>
2019-04-02 19:46:40 +0200

    uno1

--- src/house/Square.java
+++ src/house/Square.java
@@ -5,7 +5,7 @@ import java.awt.*;
 /**
  * A square that can be manipulated and that draws itself on a canvas.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael Kï¿½lling and David J. Barnes
  * @version 2016.02.29
  */
 
@@ -28,7 +28,7 @@ public class Square
         color = "red";
         isVisible = false;
     }
-
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     /**
      * Make this square visible. If it was already visible, do nothing.
      */
